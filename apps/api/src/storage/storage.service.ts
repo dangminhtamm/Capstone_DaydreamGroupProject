ea4c411e1d72c8a29beb7e07bafd2b79681c2424
Thang <s3986344@rmit.edu.vn>
2026-05-07 13:47:06 +0700

    fix supabase_url in env backend

--- apps/api/src/storage/storage.service.ts
+++ apps/api/src/storage/storage.service.ts
@@ -1,13 +1,17 @@
 // apps/api/src/storage/storage.service.ts
 import { Injectable } from '@nestjs/common';
-import { createClient } from '@supabase/supabase-js';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
 @Injectable()
 export class StorageService {
-  private supabase = createClient(
-    process.env.SUPABASE_URL !! '',
-    process.env.SUPABASE_SERVICE_KEY !! '',
-  );
+  private supabase: SupabaseClient;
+
+  constructor() {
+    this.supabase = createClient(
+      process.env.SUPABASE_URL!,
+      process.env.SUPABASE_SERVICE_KEY!
+    );
+  }
 
   async uploadFile(file: Express.Multer.File, bucket: string) {
     const filePath = `attachments/${Date.now()}-${file.originalname}`;
